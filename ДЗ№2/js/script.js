5c0cb6c071cfc0f2ed7b376bdd6614bb40ec9b5d
Evgeniya Shpak <Qxzone1@gmail.com>
2022-07-07 12:07:54 +0300

    add

---
+++ ДЗ№2/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let admin;
+const name = 'Джон';
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Земля";
+let currentUserName = 'Джон';
+
+let numberTen = 'десять' + 10;
+console.log(numberTen);
