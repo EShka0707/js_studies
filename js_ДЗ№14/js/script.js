a4d348a7160aaac33dc7f27eed4e1c99594ac191
Evgeniya Shpak <Qxzone1@gmail.com>
2022-08-01 17:13:19 +0300

    new match.pow

--- js_ДЗ№14/js/script.js
+++ js_ДЗ№14/js/script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+//1
+// for (let i = 0; i < 101; i++) {
+//     console.log(i);
+// }
+
+//2
+// for (let i = 100; i > 0; i--) {
+//     console.log(i);
+// }
+
+//3
+// for (let i = 0; i < 100; i++) {
+
+//     if(i % 2 === 0) {
+//         console.log('Четное: ', i);
+//     } else {
+//         console.log('не четное: ', i);
+//     }
+
+//     if(i >= 10) {
+//         break;
+//     }
+// }
+
+// 4
+// const arr = [1, 2, 3, 4, 5];
+// let sum = null;
+// for(let i = 0; i < arr.length; i++) {
+//     sum +=arr[i];
+// }
+// console.log(sum);
+
+//5
+// const arr = [1, 2, 3, 4, 5];
+// let sum = null;
+// for(let i = 0; i < arr.length; i++) {
+//     sum += Math.pow(arr[i], 2)
+//     // sum +=arr[i] **2;
+// }
+// console.log(sum);
