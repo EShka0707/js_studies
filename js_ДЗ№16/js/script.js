c8f88640881dbdab7fecbf0c1069f11a8645bd4b
Evgeniya Shpak <Qxzone1@gmail.com>
2022-08-05 18:39:41 +0300

    дз№16

--- js_ДЗ№16/js/script.js
+++ js_ДЗ№16/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const num = +prompt('Введите число');
+
+if (num <= 1 || isNaN(num)) {
+    console.log(NaN);
+} else {
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            console.log(i);
+            break;
+        }
+    }
+}
