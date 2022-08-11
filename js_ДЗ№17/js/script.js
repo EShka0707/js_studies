9813b8b727630e9fd62d338c6a09143e306a7cff
Evgeniya Shpak <Qxzone1@gmail.com>
2022-08-11 15:29:32 +0300

    new dz

--- js_ДЗ№17/js/script.js
+++ js_ДЗ№17/js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const num = +prompt('Введите число');
+const degree = +prompt('Ведите степень, в которую хотите возвести число');
+
+const exponentiation = (num, degree = 1) => {
+
+    if (isNaN(num) || isNaN(degree) || num === null) return confirm('какая-то ошибка');
+    return num ** degree; 
+    
+};
+const result = exponentiation(num, degree);
+alert(result);
--- js_ДЗ№18/js/script.js
+++ js_ДЗ№18/js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const padString = function (string, number, symbol, boolean = true) {
+
+    if (typeof string !== 'string') return 'error in string data';
+    if (typeof number !== 'number') return 'error in string number';
+    if (typeof symbol !== 'symbol' || symbol.length !== 1) return 'error in string symbol';
+    if (typeof boolean !== 'boolean') return 'error in string boolean';
+
+
+}
