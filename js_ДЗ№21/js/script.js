7b382b9a455915a0a0673b676262a75552a3a886
Evgeniya Shpak <Qxzone1@gmail.com>
2022-08-16 15:19:55 +0300

    new dz

--- js_ДЗ№21/js/script.js
+++ js_ДЗ№21/js/script.js
@@ -1,2 +1,18 @@
 'use strict';
+// const num = +prompt('Введите число');
+// const degree = +prompt('Ведите степень, в которую хотите возвести число');
 
+
+const mainFunction = (callback) => {
+
+    const num = +prompt('Введите число');
+    const degree = +prompt('Ведите степень, в которую хотите возвести число');
+
+    callback(num, degree);
+}
+
+const exponentiation = (num, degree) => {
+    const result = Math.pow(num, degree);
+    alert(result);
+}
+mainFunction(exponentiation);
