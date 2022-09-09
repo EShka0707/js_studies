f3eee472aab745483f0c75c49bbf5a86872cdaec
Evgeniya Shpak <Qxzone1@gmail.com>
2022-09-09 19:35:21 +0300

    Замыкания V1

--- js_ДЗ№28/js/script.js
+++ js_ДЗ№28/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function sumFunc() {
+
+    let a = 0;
+
+    return function (b) {
+        let result = a + b;
+        a += b;
+        return result;
+    }
+}
+const newSum = sumFunc();
+
+console.log(newSum(3));
+console.log(newSum(5));
+console.log(newSum(20));
