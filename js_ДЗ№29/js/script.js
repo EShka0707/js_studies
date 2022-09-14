471a69ab8d918a270fab84c12509c3329f5cc3cc
Evgeniya Shpak <Qxzone1@gmail.com>
2022-09-14 14:07:30 +0300

    new29

--- js_ДЗ№29/js/script.js
+++ js_ДЗ№29/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function randomNum () {
+
+    let userNum = [];
+
+    return function () {
+        
+        let result = Math.floor(Math.random() * 100) +1;
+
+        for (let i = 0; i < userNum.length; i += 1) {
+            if (userNum[i] === result) return;
+        }
+        userNum.push(result);
+        return result;
+    }
+    
+} 
+const func = randomNum();
+console.log(func());
