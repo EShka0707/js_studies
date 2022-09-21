7f5ce3569b22ce411f59e40aa301aab6912a2acd
Evgeniya Shpak <Qxzone1@gmail.com>
2022-09-21 19:03:12 +0300

    Переделала на стрелочные функции

--- js_ДЗ№26/js/script.js
+++ js_ДЗ№26/js/script.js
@@ -3,37 +3,32 @@
 // 3! = 3 * 2 * 1;
 
 // Task #1
-function getFactorial(n) {
+const factorial = (a) => {
+    let n = a -1;
 
-    if (n === 1 || n === 0) {
-        return 1;
-    }
+    if (n === 1 || n === 0) return 1;
 
-    return n * getFactorial(n - 1);
+    return a * factorial(n - 1);
 }
-console.log(getFactorial(3));
+console.log(factorial(3));
 
 
 // Task #2
-function pow(num, deg) {
+const pow = (a, n) => {
     
-    if(deg === 1) {
-        return num
-    }
+    if(n === 1) return a;
 
-    return num * pow(num, deg - 1);
+    return a * pow(a, n - 1);
 }
 
 console.log(pow(2, 5)); 
 
 
 // Task #3
-function sum(a, b) {
+const sum = (a, b) => {
 
-    if (b === 0) {
-        return a;
-    }
-
+    if (b === 0) return a;
+    
     return sum(a + 1, b - 1);
 }
 console.log(sum(4, 6));
