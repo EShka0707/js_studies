7308da21f2f0bb32eaef33d25da067ae69917c15
Evgeniya Shpak <Qxzone1@gmail.com>
2022-09-21 16:15:59 +0300

    Замыкания V3

--- js_ДЗ№30/js/script.js
+++ js_ДЗ№30/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const counter = (number = 0) => {
+
+    const value = () => {
+        return number;
+    }
+    const increse = () => {
+        return number += 1;
+    }
+    const decrease = () => {
+        return number -= 1;
+    }
+
+    return {
+        value,
+        increse,
+        decrease
+    }
+}
+
+const resultCounter = counter(7);
+
+console.log(resultCounter.value());
+console.log(resultCounter.increse());
+console.log(resultCounter.decrease());
