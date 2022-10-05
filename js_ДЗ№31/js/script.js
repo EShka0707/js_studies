8d7848da9867ed16fab9f9be8386a3c7dc4bb7da
Evgeniya Shpak <Qxzone1@gmail.com>
2022-10-05 14:14:47 +0300

    new bind

--- js_ДЗ№31/js/script.js
+++ js_ДЗ№31/js/script.js
@@ -10,6 +10,19 @@ function info(phone, email) {
     console.log(`Name: ${this.firstName}, tel: ${phone}, Email: ${email}`);
 }
 
+function bind(fn, context, ...rest) {
+    return function(...args) {
+        const uniqId = Date.now().toString()
+
+        context[uniqId] = fn
+
+        const result = context[uniqId](...rest.concat(args))
+
+        delete context[uniqId]
+
+        return result
+    }
+}
 const funcInfoPerson = info.bind(person, '89268400699', 'Qxzone1@gmail.com')();
 
 console.log(funcInfoPerson === info);
