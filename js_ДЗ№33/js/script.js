23d825ba436676c74782ab766f011b08586542a3
Evgeniya Shpak <Qxzone1@gmail.com>
2022-10-15 16:15:33 +0300

    This V3 Call chain

--- js_ДЗ№33/js/script.js
+++ js_ДЗ№33/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+let ladder = {
+    step: 0,
+
+    up: function() {
+      this.step++;
+      return this;
+    },
+    down: function() {
+      this.step--;
+      return this;
+    },
+    showStep: function() {
+      alert( this.step );
+      return this;
+    }
+
+};
+ladder.up().down().showStep();
+ladder.up().up().down().showStep();
+ladder.up().up().up().up().down().showStep();
