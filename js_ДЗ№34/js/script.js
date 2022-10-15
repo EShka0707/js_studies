7f5ec53b42e3b2bf04aab949c1e5c390ed10d132
Evgeniya Shpak <Qxzone1@gmail.com>
2022-10-15 19:14:18 +0300

    Замыкания V4

--- js_ДЗ№34/js/script.js
+++ js_ДЗ№34/js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const str = 'кекс кейкпопс торт донат';
+
+const insertWordInto = (str) => {
+
+    let i = 0;
+
+    return (str2) => {
+
+        const newStr = str.split(' ');
+
+        if(newStr.length < i) i = 0;
+
+        newStr.splice(i, 0, str2);
+
+        i +=1;
+
+        return newStr.join('');
+
+    }
+}
+
+const insert = insertWordInto(str);
+console.log(insert('ДЕСЕРТ'));
+console.log(insert('ДЕСЕРТ'));
+console.log(insert('ДЕСЕРТ'));
+console.log(insert('ДЕСЕРТ'));
+console.log(insert('ДЕСЕРТ'));
