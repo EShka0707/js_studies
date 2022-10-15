31838dcba60b7ee72970f2bffbeed3b8fc6c598d
Evgeniya Shpak <Qxzone1@gmail.com>
2022-10-15 15:55:17 +0300

    This v2

--- js_ДЗ№32/js/script.js
+++ js_ДЗ№32/js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+let calculator = {
+  read: function() {
+    this.a = +prompt('Enter the first number', 0);
+    this.b = +prompt('Enter the second number', 0);
+
+    if(isNaN(this.a) || isNaN(this.b)) return alert('you didn`t enter a number');
+
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  }
+};
+  
+calculator.read(5,7);
+
+alert( calculator.sum() );
+alert( calculator.mul() );
