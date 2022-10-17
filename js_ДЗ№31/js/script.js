cb15ac41ab1ac743fef9cb237624b0acdc6ac417
Evgeniya Shpak <Qxzone1@gmail.com>
2022-10-17 19:47:34 +0300

    this bind

--- js_ДЗ№31/js/script.js
+++ js_ДЗ№31/js/script.js
@@ -1,31 +1,28 @@
 'use strict';
 
-(function() {
+const person = {
+    firstName: 'Evgeniya',
+    lastName: 'Shpak',
+    age: 27
+}
 
-    const person = {
-        firstName: 'Evgeniya',
-        lastName: 'Shpak',
-        age: 27
+function info(phone, email) {
+    console.log(`Name: ${this.firstName}, tel: ${phone}, Email: ${email}`);
+}
+
+function bind(fn, context, ...rest) {
+    return function(...args) {
+        const uniqId = Date.now().toString()
+
+        context[uniqId] = fn
+
+        const result = context[uniqId](...rest.concat(args))
+
+        delete context[uniqId]
+
+        return result
     }
-    
-    function info(phone, email) {
-        console.log(`Name: ${this.firstName}, tel: ${phone}, Email: ${email}`);
-    }
-    
-    function bind(fn, context, ...rest) {
-        return function(...args) {
-            const uniqId = Date.now().toString()
-    
-            context[uniqId] = fn
-    
-            const result = context[uniqId](...rest.concat(args))
-    
-            delete context[uniqId]
-    
-            return result
-        }
-    }
-    const funcInfoPerson = info.bind(person, '89268400699', 'Qxzone1@gmail.com')();
-    
-    console.log(funcInfoPerson === info);
-})()
+}
+const funcInfoPerson = info.bind(person, '89268400699', 'Qxzone1@gmail.com')();
+
+console.log(funcInfoPerson === info);
