7892c6cffda5713f52b134c00115c4a6aa511217
Evgeniya Shpak <Qxzone1@gmail.com>
2022-11-10 12:56:29 +0300

    newDz

--- js_ДЗ№37_Dom_2/js/solution.js
+++ js_ДЗ№37_Dom_2/js/solution.js
@@ -1,16 +1,30 @@
 'use strict';
 
 (function() {
-    let bodyEl = document.body;
-    let bodyChilds = bodyEl.childNodes;
+    
+    const listUl = document.querySelector('#ulId');
+    const attrUl = listUl.attributes;
 
-    const arrLi = [];
-
-    for (let elem of li) {
-        if (elem.innerHTML !== undefined) {
-        
-            arrLi.push(elem.innerHTML)
-        }
-        
+    const arrValueAttrs = [];
+    const arrNameAttrs = [];
+    
+    for(let attrs of attrUl) {
+        arrValueAttrs.push(attrs.value);
+        arrNameAttrs.push(attrs.name);
+        console.log(attrs);
     }
-})
+    console.log(arrValueAttrs);
+    console.log(arrNameAttrs);
+
+    const lastLi = document.querySelector('li:last-child').innerHTML = 'Hello, my name is Evgeniya';
+
+    const firstLi = document.querySelector('li:first-child');
+    firstLi.setAttribute('data-my-name','Evgeniya');
+
+    const attrRemove = document.querySelector('ul');
+    attrRemove.removeAttribute("data-dog-tail");
+
+    console.log(lastLi);
+    console.log(firstLi);
+    console.log(attrRemove);
+})();
