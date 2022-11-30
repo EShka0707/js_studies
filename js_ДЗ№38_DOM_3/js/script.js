1cc7e3e5a44d9161f68002ca88109b6f49df60d6
Evgeniya Shpak <Qxzone1@gmail.com>
2022-11-30 14:28:51 +0300

    new ДЗ

--- js_ДЗ№38_DOM_3/js/script.js
+++ js_ДЗ№38_DOM_3/js/script.js
@@ -1,4 +1,29 @@
 'use strict';
 
-let person = prompt('What is your name?');
-alert("Hello, " + person + "! How are you?");
+(function() {
+
+    const listData = [1,2, [1.1,1.2,1.3, [2.1, 2.2, 2.3, [3.1, 3.2, 3.3, [4.1, 4.2, 4.3]]]] ,5];
+     
+    const recursiveList = data => {
+
+        const ul = document.createElement("ul");
+
+        for(let i = 0; i < data.length; i += 1) {
+
+            const li = document.createElement("li");
+
+            if (Array.isArray(data[i])) {
+                li.append(recursiveList(data[i]));
+            } else {
+                li.append(data[i])
+            }
+            ul.append(li);
+        }
+        return ul;
+    }
+
+    const ul = recursiveList(listData);
+    document.body.append(ul);
+})();
+
+
